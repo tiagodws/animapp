5c2e49b27151f39bb6f11ca7fd4444ab9f835472
Matheus Blödorn <blodorn.matheus@gmail.com>
2016-09-20 21:29:22 -0300

    menu

--- front-end/src/components/app.js
+++ front-end/src/components/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import MainMenu from './main-menu';
+
 export default class App extends Component {
   render() {
     return (
         <div>
-          <h1>HUE</h1>
+          <MainMenu />
           {this.props.children}
           </div>
     );
---
+++ front-end/src/components/main-menu.js
@@ -0,0 +1,30 @@
+import React, { Component} from 'react';
+import { Link } from 'react-router';
+
+export default class MainMenu extends Component{
+    render(){
+        return(
+            <div className="col-xs-12">
+                <nav className="navbar">
+                    <div className="container-fluid">
+                        <div className="navbar-header">
+                            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#menu-principal-collapse" aria-expanded="false">
+                                <span className="sr-only">Menu</span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                            </button>
+                        </div>
+                    </div>
+
+                    <div className="collapse navbar-collapse" id="menu-principal-collapse">
+                        <ul className="nav navbar-nav">
+                            <li><Link to={"/"}>Inicio</Link></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+
+        )
+    }
+}
